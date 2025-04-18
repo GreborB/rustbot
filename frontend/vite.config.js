d3731d36c2d3e5daeba745ce54452dd10c81975e
GreborB <greborb9@gmail.com>
2025-04-18 04:05:56 +0200

    Update development environment to use port 3001

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,11 +25,11 @@ export default defineConfig(({ command, mode }) => {
     },
     server: {
       host: '0.0.0.0',
-      port: 3000,
+      port: 3001,
       strictPort: true,
       hmr: {
         overlay: true,
-        clientPort: 3000,
+        clientPort: 3001,
       },
       proxy: {
         '/api': {
